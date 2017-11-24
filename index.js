3fdf7c11a3371cbf433eee427a361d7ecfc06575
IkrameM <IkrameM@users.noreply.github.com>
2017-11-24 14:43:51 +0100

    Affichage requete SQL sur localhost:3000

--- index.js
+++ index.js
@@ -1,31 +1,42 @@
 `'use strict';`,
 console.log('je suis content de faire du JS');
 /////////
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
+const mysql      = require('mysql');
+const connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : '',
   database : 'cours4'
 });
+const express = require('express');
+const app = express();
 
-connection.connect();
-
-connection.query('SELECT DISTINCT r.timeToCook FROM recipe r', function (error, results, fields) {
-  if (error) throw error;
-
-  for (var i = 0; i <results.length; i++)
-  {
-  console.log("", results[i].timeToCook); // results[0].name
-  }
-
-  /*connection.query('SELECT r.name FROM recipe r', function (error, results, fields) {
+app.get('/', function (req, res) {
+  connection.query('SELECT DISTINCT r.timeToCook FROM recipe r', function (error, results, fields) {
     if (error) throw error;
 
     for (var i = 0; i <results.length; i++)
     {
-    console.log("", results[i].name); // results[0].name
-  } */
+    console.log("", results[i].timeToCook); // results[0].name
+    }
+
+    /*connection.query('SELECT r.name FROM recipe r', function (error, results, fields) {
+      if (error) throw error;
+
+      for (const i = 0; i <results.length; i++)
+      {
+      console.log("", results[i].name); // results[0].name
+    } */
+    //variable stocké à dans la fonction res.send pour afficher tableau HTML
+res.send(results);
+  });
 
 });
-connection.end();
+
+connection.connect(function(){
+
+  app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+  });
+
+});
